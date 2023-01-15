e817114157e7904d3732a8ddd9f9c342d0b49a85
Pr1t3 <122700325+Pr1t3@users.noreply.github.com>
2023-01-15 13:35:00 +0300

    Update LifeGame.py

--- LifeGame.py
+++ LifeGame.py
@@ -1,7 +1,7 @@
 import pygame
-from gameLife import game
+from LogicOfLifeGame import game
 def drawGrid(l):
-    for row in range(0, h, blockSize):
+    for row in range(0, h-40, blockSize):
         for col in range(0, w, blockSize):
             rect = pygame.Rect(col, row, blockSize, blockSize)
             color = (100,100,100)
@@ -15,34 +15,47 @@ blockSize = 20
 sizeH = len(l)
 sizeW = len(l[0])
 w = blockSize*sizeW
-h = blockSize*sizeH
+h = blockSize*sizeH + 40
+
 
 pygame.init()
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+startButton = pygame.Rect(w/2-27.5, h-30, 55, 25)
+font = pygame.font.SysFont('Comic Sans MS', 20)
+text = font.render('Start', False, (255,255,255))
 
 isGameStarted = False
 isRunning = True
 while isRunning:
     screen.fill((0,0,0))
-    if(isGameStarted):
+    if(isGameStarted == True):
         l = game(l)
+        clock.tick(4)
+    if(isGameStarted == False):
+        pygame.draw.rect(screen, (0, 155, 0), startButton)
+        screen.blit(text, (w/2-27, h-33))
+        clock.tick(10)
     drawGrid(l)
-    clock.tick(5)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mousePos = event.pos
-            for row in range(0, h, blockSize):
-                for col in range(0, w, blockSize):
-                    mouseCol, mouseRow = mousePos
+            mouseCol, mouseRow = mousePos
+            for row in range(0, h-40, blockSize):
+                for col in range(0, w, blockSize):                    
                     if(row < mouseRow < row+blockSize):
                         if(col < mouseCol < col+blockSize):
-                            l[row//blockSize][col//blockSize] = '1'
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DELETE:
-            isGameStarted = True
+                            if(l[row//blockSize][col//blockSize] == '1'):
+                                l[row//blockSize][col//blockSize] = '0'
+                            elif(l[row//blockSize][col//blockSize] == '0'):
+                                l[row//blockSize][col//blockSize] = '1'    
+            if(310 < mouseRow < 335):
+                if(125 < mouseCol < 175):
+                    isGameStarted = True
+                    screen = pygame.display.set_mode((w, h-40))
         if event.type == pygame.QUIT:
             isRunning = False
-    
+
     pygame.display.update()
 pygame.quit()
